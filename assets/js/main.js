273ff26de96958daeb5024df2396b723f88e5220
wjdals95 <wjdals992@naver.com>
2024-04-17 15:23:35 +0900

    img 추가 css 변경 및 animation 추가와 portfolio  dataset으로 변경

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,121 +1,113 @@
-(() => {
-   const iconMenu = document.querySelector('.icon-menu');
-   const reponsiveSideBar = document.querySelector('.responsive-sidebar-menu');
-   const reponsiveSideBarOverlay = document.querySelector('.responsive-sidebar-menu .sidebar-overlay');
-   const home = document.querySelector('#home');
-   const about = document.querySelector('#about');
-   const resume = document.querySelector('#resume');
-   const mySkills = document.querySelector('#myskills');
-   const portfolio = document.querySelector('#portfolio');
-   const contact = document.querySelector('#contact');
-   const section = [home, about, resume, mySkills, portfolio, contact];
-   const sidebarNav = document.querySelectorAll('.nav-click');
-   const responsiveSidebarNav = document.querySelectorAll('.respon-nav-click');
+;(() => {
+   const home = document.querySelector('#home')
+   const about = document.querySelector('#about')
+   const resume = document.querySelector('#resume')
+   const mySkills = document.querySelector('#myskills')
+   const portfolio = document.querySelector('#portfolio')
+   const contact = document.querySelector('#contact')
+   const section = [home, about, resume, mySkills, portfolio, contact]
+   const sidebarNav = document.querySelectorAll('.nav-click')
+   const responsiveSidebarNav = document.querySelectorAll('.respon-nav-click')
 
    // ICON Toggle Event
-   function iconClick() {
-      iconMenu.addEventListener('click', () => {
-         reponsiveSideBar.classList.add('active');
-      });
-      reponsiveSideBarOverlay.addEventListener('click', e => {
-         reponsiveSideBar.classList.remove('active');
-      });
-   }
+   document.querySelector('.icon-menu').addEventListener('click', () => {
+      document.querySelector('.responsive-sidebar-menu').classList.add('active')
+   })
+   document.querySelector('.responsive-sidebar-menu .sidebar-overlay').addEventListener('click', e => {
+      document.querySelector('.responsive-sidebar-menu').classList.remove('active')
+   })
 
    // Nav Scroll Evnet
-   function navClick() {
-      for (let i = 0; i < sidebarNav.length; i++) {
-         sidebarNav[i].addEventListener('click', e => {
-            e.preventDefault();
-            window.scroll({
-               top: section[i].offsetTop,
-               behavior: 'smooth',
-            });
-         });
-      }
+   for (let i = 0; i < sidebarNav.length; i++) {
+      sidebarNav[i].addEventListener('click', e => {
+         e.preventDefault()
+         window.scroll({
+            top: section[i].offsetTop,
+            behavior: 'smooth',
+         })
+      })
+   }
 
-      for (let i = 0; i < responsiveSidebarNav.length; i++) {
-         responsiveSidebarNav[i].addEventListener('click', e => {
-            e.preventDefault();
-            window.scroll({
-               top: section[i].offsetTop,
-               behavior: 'smooth',
-            });
-            setTimeout(() => {
-               reponsiveSideBar.classList.remove('active');
-            }, 500);
-         });
-      }
+   for (let i = 0; i < responsiveSidebarNav.length; i++) {
+      responsiveSidebarNav[i].addEventListener('click', e => {
+         e.preventDefault()
+         window.scroll({
+            top: section[i].offsetTop,
+            behavior: 'smooth',
+         })
+         setTimeout(() => {
+            document.querySelector('.responsive-sidebar-menu').classList.remove('active')
+         }, 500)
+      })
    }
 
    function navScroll() {
-      let y = window.pageYOffset;
+      let y = window.pageYOffset
       for (let i = 0; i < sidebarNav.length; i++) {
-         let sectionOffsetTop = section[i].offsetTop;
-         let sectionOffsetHeight = section[i].offsetHeight;
+         let sectionOffsetTop = section[i].offsetTop
+         let sectionOffsetHeight = section[i].offsetHeight
 
          if (y >= sectionOffsetTop && y < sectionOffsetTop + sectionOffsetHeight) {
-            sidebarNav[i].classList.add('active');
+            sidebarNav[i].classList.add('active')
          } else {
-            sidebarNav[i].classList.remove('active');
+            sidebarNav[i].classList.remove('active')
          }
       }
       for (let i = 0; i < responsiveSidebarNav.length; i++) {
-         let sectionOffsetTop = section[i].offsetTop;
-         let sectionOffsetHeight = section[i].offsetHeight;
+         let sectionOffsetTop = section[i].offsetTop
+         let sectionOffsetHeight = section[i].offsetHeight
 
          if (y >= sectionOffsetTop && y < sectionOffsetTop + sectionOffsetHeight) {
-            responsiveSidebarNav[i].classList.add('active');
+            responsiveSidebarNav[i].classList.add('active')
          } else {
-            responsiveSidebarNav[i].classList.remove('active');
+            responsiveSidebarNav[i].classList.remove('active')
          }
       }
    }
 
    // Go Portfilio Btn Event
    document.querySelector('.go-portfolio-btn').addEventListener('click', e => {
-      e.preventDefault();
+      e.preventDefault()
 
       window.scroll({
          top: portfolio.offsetTop,
          behavior: 'smooth',
-      });
-   });
-
-   const portfolioDetailItems = document.querySelectorAll('.porfolio-detail-item');
-   const portfolioDetailBox = document.querySelector('.portfolio-detail');
-   const porfolioPrevBtn = document.querySelector('.portfolio-detail .prev');
-   const porfolioNextBtn = document.querySelector('.portfolio-detail .next');
+      })
+   })
 
    // start: Portfolio
    document.querySelectorAll('[data-toggle="portfolio-item"]').forEach(el => {
       el.addEventListener('click', function (e) {
-         e.preventDefault();
+         e.preventDefault()
 
-         document.querySelector('.portfolio-detail').classList.add('show');
+         document.querySelector('.portfolio-detail').classList.add('show')
 
-         document.querySelectorAll('.porfolio-detail-item').forEach(item => item.classList.remove('active'));
-         document.querySelector(this.dataset.target).classList.add('active');
-      });
-   });
+         document.querySelectorAll('.porfolio-detail-item').forEach(item => item.classList.remove('active'))
+         document.querySelector(this.dataset.target).classList.add('active')
+      })
+   })
 
    document.querySelector('.portfolio-detail .close').addEventListener('click', () => {
-      portfolioDetailBox.classList.remove('show');
-      portfolioDetailItems.classList.remove('active');
-   });
+      document.querySelector('.portfolio-detail').classList.remove('show')
+      document.querySelectorAll('.porfolio-detail-item').classList.remove('active')
+   })
 
    document.querySelector('.portfolio-detail .slider-overlay').addEventListener('click', () => {
-      portfolioDetailBox.classList.remove('show');
-      portfolioDetailItems.classList.remove('active');
-   });
+      document.querySelector('.portfolio-detail').classList.remove('show')
+      document.querySelectorAll('.porfolio-detail-item').classList.remove('active')
+   })
    // end: Portfolio
 
    window.addEventListener('click', e => {
-      iconClick();
-      navClick();
-   });
+      navClick()
+   })
 
    window.addEventListener('scroll', () => {
-      setTimeout(navScroll, 500);
-   });
-})();
+      setTimeout(navScroll, 500)
+   })
+   window.addEventListener('load', () => {
+      document.querySelector('.myinfo-card').classList.add('intro')
+      document.querySelector('.sidebar-nav').classList.add('intro')
+      document.querySelector('.home-section .custom-container').classList.add('intro')
+   })
+})()
